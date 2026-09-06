2269982a05d273d85e2f7750f757e9f22ffb8c14
bhavya09mang-droid <310924031+bhavya09mang-droid@users.noreply.github.com>
2026-09-06 17:13:09 +0530

    Add files via upload

---
+++ tw.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // This looks for HTML and JS files in your root folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
